2e60b3527d4ce5cc6d3ec99156f7da74f0607a8e
Sayali Mhalaskar <143711818+Sayali-mhalaskar@users.noreply.github.com>
2026-05-10 15:20:23 +0000

    Embedded Linux ARM Experiment Completed

---
+++ arm_app.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Embedded Linux ARM Application Started\n");
+    printf("Cross Compilation Successful\n");
+    printf("Application Running in ARM Environment\n");
+    return 0;
+}
